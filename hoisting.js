b2a5dc926a9064dd50dbda675cd1fe795b271ace
vipul tapare <vipultapare12@gmail.com>
2024-04-01 16:54:43 +0530

    updated js file

--- hoisting.js
+++ hoisting.js
@@ -2,6 +2,7 @@
 getName(); // Namaste React
 console.log(x);  // undefined  if we remove the var x = 7 then it will throw the error.
 var x = 7;
+ // get()  - will throw the error like it is not a function , it will behave like a variable.
 
 function getName(){
   console.log("Namaste React");
@@ -12,3 +13,8 @@ console.log(x); //7
 // This is called Hoisting:
 //Even before executing the code it first store the memory for the variables and functions.
 
+var get = ()=>{
+  console.log("hii");
+}
+
+get();  //now it will execute the code.
