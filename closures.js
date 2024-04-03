f3a3d81977f0e1dddc6c93fcc925a0edac5ebea3
vipul tapare <vipultapare12@gmail.com>
2024-04-03 11:10:00 +0530

    Added Defination

--- closures.js
+++ closures.js
@@ -13,3 +13,7 @@ console.log(z); // output : [function :y]
 
 //function along with lexical scope called closure.
 //A closure is the combination of a function and the variables that are accessible from that function.
+
+z(); // output: 7  ... When we return the function only function is not return it will also return the lexical scope of each function with the variable scope this is called closures.
+
+// Function along with its lexical scope bundle together forms a closure ---- Important
